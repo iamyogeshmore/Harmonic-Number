49b983bd313e9175e2dcb2c4ab33f92a8b1aca4f
Yogesh More <yogesh.more2098@gmail.com>
2023-06-08 03:10:51 +0530

    Added:Prints the Nth harmonic number: 1/1 + 1/2 + ... + 1/N.

---
+++ src/main/java/org/bridgelabz/HarmonicNumber.java
@@ -0,0 +1,21 @@
+package org.bridgelabz;
+
+import java.util.Scanner;
+
+// Prints the Nth harmonic number: 1/1 + 1/2 + ... + 1/N
+public class HarmonicNumber {
+    public static void main(String[] args) {
+        System.out.print("Enter any number : ");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        double result = 0.0;
+        if (num != 0) {
+            while (num > 0) {
+                result = result + (double) 1 / num;
+                num--;
+            }
+            System.out.println("Total of Harmonic Series is " + result);
+        } else
+            System.out.println("Invalid input");
+    }
+}
